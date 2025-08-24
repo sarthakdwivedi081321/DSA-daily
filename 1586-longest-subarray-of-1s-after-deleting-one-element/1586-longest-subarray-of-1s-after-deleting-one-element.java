2da9e098a19e01d3a62669f96f9a89a8808a27e4
SARTHAK DWIVEDI <135525211+sarthakdwivedi081321@users.noreply.github.com>
2025-08-24 23:26:38 +0530

    Time: 3 ms (77.92%), Space: 57 MB (48.74%) - LeetHub

--- 1586-longest-subarray-of-1s-after-deleting-one-element/1586-longest-subarray-of-1s-after-deleting-one-element.java
+++ 1586-longest-subarray-of-1s-after-deleting-one-element/1586-longest-subarray-of-1s-after-deleting-one-element.java
@@ -1,40 +1,26 @@
 class Solution {
     public int longestSubarray(int[] nums) {
-        int n=nums.length;
-        int lft=0,cnt=0,j=0,k=0,mx=0,l=0;
-        for(int i=0;i<n;i++){
-            if(nums[i]==1){
-                lft=i;
-                j=i;
-                break;
-            }
-        }
-        for(int rgt=j;rgt<n;rgt++){
-            while(nums[rgt]==0 && cnt==1 && lft<n){
-                mx=Math.max(mx,rgt-lft-cnt);
-                if(k!=0){
-                    lft=k+1;
-                    k=0;
-                }
-                if(nums[lft]==1){
-                    cnt=0;
-                    break;
-                }
-                lft++;
-            }
-            if(nums[rgt]==0){
-                l=1;
-                cnt=1;
-                k=rgt;
-            }
-        }
-        if(l==0 && j!=0){
-            mx=Math.max(mx,(n-lft));
+        int left = 0;
+        int zeroCount = 0;
+        int maxLen = 0;
 
+        for (int right = 0; right < nums.length; right++) {
+            if (nums[right] == 0) {
+                zeroCount++;
+            }
+
+            // If more than one zero in window, shrink from the left
+            while (zeroCount > 1) {
+                if (nums[left] == 0) {
+                    zeroCount--;
+                }
+                left++;
+            }
+
+            // We must delete one element, so result is window size - 1
+            maxLen = Math.max(maxLen, right - left);
         }
-        mx=Math.max(mx,((n-1)-lft));
-        return mx;
-        
-        
+
+        return maxLen;
     }
 }
