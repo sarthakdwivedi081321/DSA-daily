21b8de6c84c0af4d9afaf5bb4b8ce661461a16f2
SARTHAK DWIVEDI <135525211+sarthakdwivedi081321@users.noreply.github.com>
2025-09-05 19:16:43 +0530

    Time: 6 ms (98.74%), Space: 48.2 MB (34.95%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,15 +1,15 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
         HashMap<String,List<String>> map=new HashMap<>();
-        int n=strs.length;
         for(String str:strs){
-            int n1=str.length();
             char ch[]=str.toCharArray();
             Arrays.sort(ch);
-            String str1=new String(ch);
-            map.computeIfAbsent(str1,k->new ArrayList<>()).add(str);
+            String st=new String(ch);
+            map.computeIfAbsent(st,k->new ArrayList<>()).add(str);
+
 
         }
         return new ArrayList<>(map.values());
+        
     }
 }
