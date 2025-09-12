6b60402b501d37b8144186a8a2492b680318511e
SARTHAK DWIVEDI <135525211+sarthakdwivedi081321@users.noreply.github.com>
2025-09-12 11:40:19 +0530

    Time: 88 ms (92.02%), Space: 50.1 MB (16.53%) - LeetHub

--- 0204-count-primes/0204-count-primes.java
+++ 0204-count-primes/0204-count-primes.java
@@ -1,16 +1,16 @@
 class Solution {
     public int countPrimes(int n) {
         boolean ans[]=new boolean[n];
-        for(int i=2;i*i<n;i++){
-            if(!ans[i]){
-                for(int j=i*2;j<n;j+=i){
-                    ans[j]=true;
+        for(int j=2;j*j<n;j++){
+            if(!ans[j]){
+                for(int k=j*2;k<n;k+=j){
+                    ans[k]=true;
                 }
             }
         }
         int cnt=0;
-        for(int k=2;k<n;k++){
-            if(!ans[k]){
+        for(int l=2;l<n;l++){
+            if(!ans[l]){
                 cnt+=1;
             }
         }
