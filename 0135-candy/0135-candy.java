b8ab56ea5217de7efe46bdc4b99b4af351cd65d2
SARTHAK DWIVEDI <135525211+sarthakdwivedi081321@users.noreply.github.com>
2025-09-26 11:43:46 +0530

    Time: 3 ms (80.19%), Space: 45.8 MB (57.6%) - LeetHub

--- 0135-candy/0135-candy.java
+++ 0135-candy/0135-candy.java
@@ -1,20 +1,21 @@
 class Solution {
     public int candy(int[] ratings) {
         int n=ratings.length;
-        int arr1[]=new int[n];
-        Arrays.fill(arr1,1);
+        int arr[]=new int[n];
+        Arrays.fill(arr,1);
         for(int i=1;i<n;i++){
             if(ratings[i]>ratings[i-1]){
-                arr1[i]=arr1[i-1]+1;
+                arr[i]=arr[i-1]+1;
+
             }
         }
         for(int j=n-2;j>=0;j--){
             if(ratings[j]>ratings[j+1]){
-                arr1[j]=Math.max(arr1[j+1]+1,arr1[j]);
+                arr[j]=Math.max(arr[j+1]+1,arr[j]);
             }
         }
         int cnt=0;
-        for(int i:arr1){
+        for(int i:arr){
             cnt+=i;
         }
         return cnt;
