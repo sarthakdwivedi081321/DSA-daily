c3ac7159b3ff1216f7c05839702cadfa40c365d2
SARTHAK DWIVEDI <135525211+sarthakdwivedi081321@users.noreply.github.com>
2025-10-13 22:37:19 +0530

    Time: 3 ms (55.46%), Space: 44.9 MB (33.97%) - LeetHub

--- 1353-find-resultant-array-after-removing-anagrams/1353-find-resultant-array-after-removing-anagrams.java
+++ 1353-find-resultant-array-after-removing-anagrams/1353-find-resultant-array-after-removing-anagrams.java
@@ -3,7 +3,6 @@ class Solution {
         List<String> ans=new ArrayList<>();
         List<String> ans1=new ArrayList<>();
         int l=0;
-        StringBuilder st1=new StringBuilder(words[0]);
         for(int i=0;i<words.length;i++){
             char ch[]=words[i].toCharArray();
             Arrays.sort(ch);
@@ -16,12 +15,6 @@ class Solution {
                 ans1.add(st);
                 l++;
             }
-        }
-        String arr[]=new String[ans.size()];
-        int j=0;
-        for(String str1:ans){
-            arr[j]=str1;
-            j++;
         }
         return ans;
         
